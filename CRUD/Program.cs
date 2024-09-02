2f5b1584859255d78bffec1ff1d9b48976bf8e7d
mhmdwalibaba <mhmdlybaba191@gmail.com>
2024-09-02 16:28:06 +0330

    add MiddleWares IN Program.cs for Enable and maching Routing with UseRoutingMiddleWare,excuting action in conroller with useEndpoints

--- CRUD/Program.cs
+++ CRUD/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
