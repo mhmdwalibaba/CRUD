c3577361b1692c39c5d33b00934001de22fe4c40
mhmdwalibaba <mhmdlybaba191@gmail.com>
2024-09-02 17:06:20 +0330

    Create Model Country in Entits and Create Dto for Country in servceContracts

---
+++ ServiceContract/DTO/CountryAddRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.DTO
+{
+    public  class CountryAddRequest
+    {
+        public string? Country { get; set; }
+    }
+}
