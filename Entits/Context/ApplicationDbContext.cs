246d4520d687293c3722312832d4e48fa1ebe6a4
mhmdwalibaba <mhmdlybaba191@gmail.com>
2024-09-02 20:49:49 +0330

    Create DatabaseSql for Countries date with efcore code first

---
+++ Entits/Context/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace Entits
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Country> countries { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().ToTable("countries");
+
+            //seed to Countries
+            string countriesJson = System.IO.File.ReadAllText("countries.json");
+            List<Country> countries=System.Text.Json.JsonSerializer.Deserialize<List<Country>>(countriesJson);
+
+            foreach(Country country in countries)
+            {
+                modelBuilder.Entity<Country>().HasData(country);
+            }
+        }
+    }
+}
