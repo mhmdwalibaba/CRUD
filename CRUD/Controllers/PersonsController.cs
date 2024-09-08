116dbb926ef6e586e341e04844a49b60e184a9c1
mhmdwalibaba <mhmdlybaba191@gmail.com>
2024-09-08 07:10:33 +0330

    I fixed some errors

---
+++ CRUD/Controllers/PersonsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Enum;
+using Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CRUD.Controllers
+{
+    [Route("[controller]")]
+    public class PersonsController : Controller
+    {
+        private readonly IPersonsService _personsService;
+        private readonly ICountriesService _countriesService;
+        public PersonsController(IPersonsService personsService,ICountriesService countriesService)
+        {
+            _personsService = personsService;
+            _countriesService = countriesService;
+        }
+
+        [Route("/")]
+        
+        [Route("/persons/index")]
+        public async Task<IActionResult> Index([FromQuery] string? searchBy, [FromQuery] string? searchString,string sortBy,SortOrderOptions sortOrder=SortOrderOptions.ASC)
+        {
+            ViewBag.SearchFields = new Dictionary<string,string>()
+            {
+                {nameof(PersonResponse.PersonName),"PersonName" },
+                {nameof(PersonResponse.Email),"Email" },
+                {nameof(PersonResponse.Gender),"Gender" },
+                {nameof(PersonResponse.Address),"Address" },
+                {nameof(PersonResponse.CountryID),"Country" },
+                {nameof(PersonResponse.DateOfBirth),"DateOfBirth" }
+            };
+           List<PersonResponse>? persons =await  _personsService.GetFiltredPerson(searchBy,searchString);
+           ViewBag.CurrentSearchString=searchString;
+            ViewBag.CurrentSearchBy = searchBy;
+
+            //sort
+            List<PersonResponse>? sortedPerson = await _personsService.GetSortedPersons(persons,sortBy, sortOrder);
+            ViewBag.CurrentSortBy=sortBy;
+            ViewBag.CurrentSortOrder = sortOrder.ToString();
+
+            return View(sortedPerson);
+        }
+        [HttpGet]
+        [Route("/persons/create")]
+        public async Task<IActionResult> Create()
+        {
+            List<CountryResponse?> countries = await _countriesService.GetAllCountries();
+            ViewBag.Countries = countries.Select(temp =>
+               new SelectListItem() { Text = temp.CountryName, Value = temp.CountryID.ToString() }
+             );
+            return View();
+        }
+        [HttpPost]
+        [Route("/persons/create")]
+        
+        public async Task<IActionResult> Create(PersonAddRequest personAddRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                List<CountryResponse?> countries = await _countriesService.GetAllCountries();
+                ViewBag.Countries = countries.Select(temp =>
+                new SelectListItem() { Text = temp.CountryName, Value = temp.CountryID.ToString() });
+
+                ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                return View();
+            }
+
+            //call the service method
+            PersonResponse? personResponse = await _personsService.AddPerson(personAddRequest);
+
+            //navigate to Index() action method (it makes another get request to "persons/index"
+            return RedirectToAction("Index", "Persons");
+
+            
+        }
+    }
+}
--- ServiceContract/DTO/PersonAddRequest.cs
+++ ServiceContract/DTO/PersonAddRequest.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ServiceContracts.Enum;
 using Entits;
 namespace ServiceContracts.DTO
 {
     public class PersonAddRequest
     {
+        [Required(ErrorMessage = "Person Name can't be blank")]
         public string? PersonName { get; set; }
+
+        [Required(ErrorMessage = "Email can't be blank")]
+        [EmailAddress(ErrorMessage = "Email value should be a valid email")]
+        [DataType(DataType.EmailAddress)]
         public string? Email { get; set; }
+
+
+        [Required(ErrorMessage = "Please select gender of the person")]
         public GenderOptions? Gender { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime? DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Please select a country")]
         public Guid CountryID { get; set; }
         public string? Address { get; set; }
         public bool ReceiveNewsLetters { get; set; }
